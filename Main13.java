07b41df85a7d47159438ecfdc108e2b8b0ca678e
Navyasreepalagiri2004 <navyasreepalagiri@gmail.com>
2025-06-02 15:37:49 +0500

    Add files via upload

---
+++ Main13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ public class Main13{
+  public static void main(String[] args){
+   Scanner sc=new Scanner(System.in);
+   String a=sc.next();
+   String b=" ";
+   for(int i=a.length()-1;i>-1;i--){
+   b=b+a.charAt(i);
+   }
+   System.out.println(b);
+   
+   }
+   }
+   
