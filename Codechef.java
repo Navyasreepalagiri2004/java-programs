3d3d100d4e33963ec1edf8c8e335672049f94b7b
Navyasreepalagiri2004 <navyasreepalagiri@gmail.com>
2025-06-17 14:58:17 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner; 
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int R=sc.nextInt();
+		int B=sc.nextInt();
+		int P=sc.nextInt();
+		int Q=sc.nextInt();
+	
+		   int s=R*P;
+		   int m=B*Q;
+		
+		System.out.println(s);
+		System.out.println(m);
+        sc.close();
+	}
+}
